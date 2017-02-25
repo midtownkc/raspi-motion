4dc1fc553fd73ff11722c9058a8055767abd6449
midtownkc <maxwell.frederickson@gmail.com>
2017-02-25 00:52:04 -0600

    Update motionDetector.py

--- motionDetector.py
+++ motionDetector.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 # This program uses a motion detector, takes a pic and
 # sends an SMS if motion is detected.
+# http://i.imgur.com/P2MaW4H.jpg
 # Tested on Raspberry Pi 3, No-IR Filter PiCamera 2.0
 # Shout out to Mike Haldas for the sms & imgur code
 # http://bit.ly/2ljRJqc
 # Account credentials masked ~ ~ 
 # maxwell [d0t] frederickson [@t] gmail.com for ???'s
+# I still need to add a wait between the pictures
+# Since Twilio limits to 250 SMS a month -- maybe FB Messenger?
 #
 import pyimgur
 import picamera
